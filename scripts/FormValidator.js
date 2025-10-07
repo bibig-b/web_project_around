6245c79cd6ac06b91773a3cd27b956b48998402b
bibig-b <beatriz199313@yahoo.com>
2025-10-07 14:58:32 +0100

    atualização FormValidator

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -1,79 +1,91 @@
 // Função para mostrar mensagem de erros
-function showInputError(formElement, inputElement, errorMessage) {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.add("pop-up__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("pop-up__error_visible");
-}
+class FormValidator {
+  constructor(settings, formElement) {
+    this._settings = settings;
+    this._formElement = formElement;
+  }
+  #showInputError(inputElement, errorMessage) {
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.add("pop-up__input_type_error");
+    errorElement.textContent = errorMessage;
+    errorElement.classList.add("pop-up__error_visible");
+  }
 
-// Função para esconder mensagens de erros
-function hideInputError(formElement, inputElement) {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove("pop-up__input_type_error");
-  errorElement.classList.remove("pop-up__error_visible");
-  errorElement.textContent = "";
-}
+  #hideInputError(inputElement) {
+    // Função para esconder mensagens de erros
+    const errorElement = this._formElement.querySelector(
+      `#${inputElement.id}-error`
+    );
+    inputElement.classList.remove("pop-up__input_type_error");
+    errorElement.classList.remove("pop-up__error_visible");
+    errorElement.textContent = "";
+  }
 
-// Função para verificar a validade do input
-function isValid(formElement, inputElement) {
-  if (!inputElement.validity.valid) {
-    //campo inválido - mostrar o erro
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    //campo válido - esconder o erro
-    hideInputError(formElement, inputElement);
+  #isValid(inputElement) {
+    // Função para verificar a validade do input
+    if (!inputElement.validity.valid) {
+      //campo inválido - mostrar o erro
+      this.#showInputError(inputElement, inputElement.validationMessage);
+    } else {
+      //campo válido - esconder o erro
+      this.#hideInputError(inputElement);
+    }
+  }
+
+  #hasInvalidInput(inputList) {
+    // Função para verificar se há algum input inválido
+    return inputList.some((inputElement) => {
+      return !inputElement.validity.valid;
+    });
+  }
+
+  #toggleButtonState(inputList, buttonElement) {
+    // Função para ativar ou desativar o botão de envio
+    if (this.#hasInvalidInput(inputList)) {
+      buttonElement.classList.add("pop-up__submit_disabled");
+      buttonElement.disabled = true;
+    } else {
+      buttonElement.classList.remove("pop-up__submit_disabled");
+      buttonElement.disabled = false;
+    }
+  }
+
+  #setEventListeners() {
+    // Função para adicionar os event listeners aos inputs do formulário
+    const inputList = Array.from(
+      this._formElement.querySelectorAll(".pop-up__input")
+    );
+    const buttonElement = this._formElement.querySelector(".pop-up__submit");
+
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this.#isValid(inputElement);
+        this.#toggleButtonState(inputList, buttonElement);
+      });
+    });
+  }
+
+  enableValidation() {
+    // Função para habilitar a validação
+    const formList = Array.from(document.querySelectorAll(".pop-up__form"));
+    formList.forEach((formElement) => {
+      formElement.addEventListener("submit", function (evt) {
+        evt.preventDefault();
+      });
+      this.#setEventListeners(formElement);
+    });
   }
 }
 
-// Função para verificar se há algum input inválido
-function hasInvalidInput(inputList) {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  });
-}
+export default FormValidator;
 
-// Função para ativar ou desativar o botão de envio
-function toggleButtonState(inputList, buttonElement) {
-  if (hasInvalidInput(inputList)) {
-    buttonElement.classList.add("pop-up__submit_disabled");
-    buttonElement.disabled = true;
-  } else {
-    buttonElement.classList.remove("pop-up__submit_disabled");
-    buttonElement.disabled = false;
-  }
-}
-
-// Função para adicionar os event listeners aos inputs do formulário
-function setEventListeners(formElement) {
-  const inputList = Array.from(formElement.querySelectorAll(".pop-up__input"));
-  const buttonElement = formElement.querySelector(".pop-up__submit");
-
-  toggleButtonState(inputList, buttonElement);
-
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener("input", function () {
-      isValid(formElement, inputElement);
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-}
-
-// Função para habilitar a validação
-function enableValidation() {
-  const formList = Array.from(document.querySelectorAll(".pop-up__form"));
-  formList.forEach((formElement) => {
-    formElement.addEventListener("submit", function (evt) {
-      evt.preventDefault();
-    });
-    setEventListeners(formElement);
-  });
-}
-
-enableValidation({
+/*enableValidation({
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
   submitButtonSelector: ".popup__submit",
   inactiveButtonClass: "popup__button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
-});
+});*/
